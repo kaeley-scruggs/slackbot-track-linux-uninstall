f72e203eb35c19661d099ad09d638ffa825568af
Kaeley Scruggs <kaeley.scruggs@getweave.com>
2025-09-09 09:30:45 -0600

    update with row id and made username unique for tables

--- create_db.py
+++ create_db.py
@@ -1,32 +1,37 @@
 import sqlite3
+import os
+import secrets
 
-conn = sqlite3.connect("tally_db.db")
-
+conn = sqlite3.connect(secrets.db_path)
 my_cursor = conn.cursor()
 
 # create the table for Linux
 def create_linux_table():
     my_cursor.execute("""CREATE TABLE Linux_Reinstall (
+                        lr_id INTEGER PRIMARY KEY,
                         display_name text,
-                        username text, 
+                        username text UNIQUE, 
                         last_date text,
                         last_time text,
-                        count integer
+                        reinstall_count integer
                         )
         """)
 
 # create the table for 1Password
 def create_1pass_table():
     my_cursor.execute("""CREATE TABLE '1Password_Recovery' (
+                        op_id INTEGER PRIMARY KEY,
                         display_name text,
-                        username text, 
+                        username text UNIQUE, 
                         last_date text,
                         last_time text,
-                        count integer
+                        recovery_ count integer
                         )
         """)
-my_cursor.execute("INSERT INTO Linux_Reinstall VALUES ('kaeley', '<@UDSERSAS9D>', '2025-08-05', '08:25:18.922434', 2)")
+# create_linux_table()
+# create_1pass_table()
+'''my_cursor.execute("INSERT INTO Linux_Reinstall VALUES ('kaeley', '<@UDSERSAS9D>', '2025-08-05', '08:25:18.922434', 2)")
 my_cursor.execute("INSERT INTO Linux_Reinstall VALUES ('test', '<@TEST>', '2025-08-02', '08:25:18.922434', 2)")
-my_cursor.execute("INSERT INTO Linux_Reinstall VALUES ('testing', '<@TESTING>', '2025-07-15', '08:25:18.922434', 2)")
+my_cursor.execute("INSERT INTO Linux_Reinstall VALUES ('testing', '<@TESTING>', '2025-07-15', '08:25:18.922434', 2)")'''
 conn.commit()
 conn.close()
